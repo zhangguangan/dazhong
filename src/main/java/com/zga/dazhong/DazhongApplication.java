33d70b6f2bf505d3ad2617340cbe17fc010e0d06
张广安 <zhangguangan@odianyun.com>
2020-02-03 15:12:32 +0800

    first commit

---
+++ src/main/java/com/zga/dazhong/DazhongApplication.java
@@ -0,0 +1,13 @@
+package com.zga.dazhong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DazhongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DazhongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zga/dazhong/dazhong/DazhongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zga.dazhong.dazhong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DazhongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
