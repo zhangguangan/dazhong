e675a5256067219d8f219dcf6e1ab24d5bc92c04
zhangguangan-jk <zhangguangan-jk@360jinrong.net>
2020-11-25 21:16:14 +0800

    r

--- src/main/java/com/zga/dazhong/DazhongApplication.java
+++ src/main/java/com/zga/dazhong/DazhongApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 @PropertySource({"classpath:application.properties", "classpath:spring-redis.properties"})
 @MapperScan("com.zga.dazhong.wxs.mapper")
-@EnableApolloConfig
+//@EnableApolloConfig
 public class DazhongApplication {
 
     public static void main(String[] args) {
